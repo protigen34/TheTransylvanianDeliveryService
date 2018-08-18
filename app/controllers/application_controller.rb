245cb1cdbac35d98264c5d4c56ff19731578bf20
EC2 Default User <ec2-user@ip-172-31-35-74.us-east-2.compute.internal>
2018-08-18 06:32:53 +0000

    4.3 controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
-class ApplicationController < ActionController::Base
+class OrdersController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
 end
