bc63eac546c747414a9bc33daf4fc3c8f6cd9884
EC2 Default User <ec2-user@ip-172-31-35-74.us-east-2.compute.internal>
2018-08-20 09:11:25 +0000

    4.4 Models & Associations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,1 @@
-class OrdersController < ApplicationController
-  def index
-  end
-
-  def show
-  end
-
-  def new
-  end
-
-  def create
-  end
-
-  def destroy
-  end
-end
+class ApplicationRecord < ActiveRecord::Base
