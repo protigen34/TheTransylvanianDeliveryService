4390d665817fdfbaef3653ff02600042f0bc7e1c
EC2 Default User <ec2-user@ip-172-31-35-74.us-east-2.compute.internal>
2018-08-22 22:52:27 +0000

    4.3 & 4.4

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_22_021900) do
+ActiveRecord::Schema.define(version: 2018_08_21_220208) do
 
   create_table "orders", force: :cascade do |t|
     t.integer "user_id"
@@ -18,15 +18,6 @@ ActiveRecord::Schema.define(version: 2018_08_22_021900) do
     t.float "total"
     t.index ["product_id"], name: "index_orders_on_product_id"
     t.index ["user_id"], name: "index_orders_on_user_id"
-  end
-
-  create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
-    t.integer "category_id"
-    t.integer "author_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "products", force: :cascade do |t|
