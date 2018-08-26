1979fb62acbb549092b2816140cb72d2ffe9006c
EC2 Default User <ec2-user@ip-172-31-35-74.us-east-2.compute.internal>
2018-08-26 02:33:04 +0000

    5.1 create routes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,9 +8,19 @@ class PostsController < ApplicationController
         
     end
     
-    def create
-        
+def create
+  @product = Product.new(product_params)
+
+  respond_to do |format|
+    if @product.save
+      format.html { redirect_to @product, notice: 'Product was successfully created.' }
+      format.json { render :show, status: :created, location: @product }
+    else
+      format.html { render :new }
+      format.json { render json: @product.errors, status: :unprocessable_entity }
     end
+  end
+end
         
     def
     
