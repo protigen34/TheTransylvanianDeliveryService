687726b9c4832ddd7b002bce78ec6fb826e866d8
EC2 Default User <ec2-user@ip-172-31-35-74.us-east-2.compute.internal>
2018-09-01 09:05:43 +0000

    5.11 Consumer Web App

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,7 +4,13 @@ class UserMailer < ApplicationMailer
   def contact_form(email, name, message)
   @message = message
     mail(from: email,
-         to: 'your-email@example.com',
-         subject: "A new contact form message from #{name}")
+         to: 'somepleb271@yahoo.com',
+         subject: "A new contact form message from CJ")
+  end
+  
+  def welcome(user)
+  @appname = "Bike Shop"
+  mail(to: user.email,
+       subject: "Welcome to #{@appname}!")
   end
 end
