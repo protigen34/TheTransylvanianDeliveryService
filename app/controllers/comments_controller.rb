d9879de8b19f79d6f46966379ed822248fd4dd98
EC2 Default User <ec2-user@ip-172-31-35-74.us-east-2.compute.internal>
2018-09-07 06:03:56 +0000

    5.10 Admin Moderating Comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,34 +1,34 @@
 class CommentsController < ApplicationController
 
-def create
-  @product = Product.find(params[:product_id])
-  @comment = @product.comments.new(comment_params)
-  @comment.user = current_user
-
-  respond_to do |format|
-    if @comment.save
-      format.html { redirect_to @product, notice: 'Review was created successfully.' }
-      format.json { render :show, status: :created, location: @product }
-    else
-      format.html { redirect_to @product, alert: 'Review was not saved successfully.' }
-      format.json { render json: @comment.errors, status: :unprocessable_entity }
+  def create
+    @product = Product.find(params[:product_id])
+    @comment = @product.comments.new(comment_params)
+    @comment.user = current_user
+    @user = current_user
+    respond_to do |format|
+      if @comment.save
+        ActionCable.server.broadcast 'product_channel', comment: @comment, average_rating: @comment.product.average_rating
+        format.html { redirect_to @product, notice: 'Review was created successfully.' }
+        format.json { render :show, status: :created, location: @product }
+        format.js
+      else
+        format.html { redirect_to @product, alert: 'Review was not saved successfully.' }
+        format.json { render json: @comment.errors, status: :unprocessable_entity }
+      end
     end
   end
-end
 
-  private
-    def comment_params
-      params.require(:comment).permit(:user_id, :body, :rating)
-    end
-end
+  def destroy
+    @comment = Comment.find(params[:id])
+    product = @comment.product
+    @comment.destroy
+    redirect_to product
+  end
 
-def create
-end
+private
 
-def destroy
-  @comment = Comment.find(params[:id])
-  product = @comment.product
-  @comment.destroy
-  redirect_to product
-end
+  def comment_params
+    params.require(:comment).permit(:user_id, :body, :rating)
+  end
 
+end
