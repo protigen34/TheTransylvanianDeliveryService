212b2b4579e6645c47a0de31b29373ba6de3df2e
EC2 Default User <ec2-user@ip-172-31-35-74.us-east-2.compute.internal>
2018-09-21 10:43:20 +0000

    4.7 Dynamic Rails Web App

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,28 +1,25 @@
 class Product < ApplicationRecord
-	validates :name, presence: true
-    validates :description, presence: true
-    validates :color, presence: true
-    validates :price, presence: true
-    has_many :orders
+    validates :name, presence: true
+    has_many :orders 
     has_many :comments
     
-	def highest_rating_comment
-  		comments.rating_desc.first
-	end
-	def lowest_rating_comment
-  		comments.rating_asc.first
-	end
-	def average_rating
-	  comments.average(:rating).to_f
-	end
+    def highest_rating_comment
+        comments.rating_desc.first
+    end
+    def lowest_rating_comment
+  	    comments.rating_asc.first
+    end
+    def average_rating
+  	    comments.average(:rating).to_f
+    end
 
-	def self.search(search_term)
-      if Rails.env.production?
-        Product.where("name ilike ?", "%#{search_term}%")
-      else
-        Product.where("name LIKE ?", "%#{search_term}")
-      end
-	end
+    def self.search(search_term)
+        if Rails.env.production?
+            Product.where("name ilike ?", "%#{search_term}%")
+        else
+            Product.where("name LIKE ?", "%#{search_term}%")
+        end
+    end
 end
 
 
