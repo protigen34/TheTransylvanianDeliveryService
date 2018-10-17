edcb21070083bf5a627b9be75e7a6e1554859bd1
EC2 Default User <ec2-user@ip-172-31-42-163.us-east-2.compute.internal>
2018-10-17 03:01:41 +0000

    sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -108,4 +108,14 @@ Rails.application.configure do
   # Configure heroku for using actionCable
   config.web_socket_server_url = "wss://charliejin.herokuapp.com/cable"
   config.action_cable.allowed_request_origins = ['https://charliejin.herokuapp.com', 'http://charliejin.herokuapp.com']
+  
+  ActionMailer::Base.smtp_settings = {
+    address: 'smtp.sendgrid.net',
+    port: '587',
+    authentication: :plain,
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
+    domain: 'heroku.com',
+    enable_starttls_auto: true
+  }
 end
