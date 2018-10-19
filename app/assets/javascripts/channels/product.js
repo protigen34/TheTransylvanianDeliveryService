4be89bde5ceef9826992d6208eb66f9f7ea8d6a7
EC2 Default User <ec2-user@ip-172-31-42-163.us-east-2.compute.internal>
2018-10-19 22:25:05 +0000

    product.js

--- app/assets/javascripts/channels/product.js
+++ app/assets/javascripts/channels/product.js
@@ -1,23 +1,23 @@
-App.product = App.cable.subscriptions.create("ProductChannel",
-  {connected: function() {},
-    // Called when the subscription is ready for use on the server
+App.product = App.cable.subscriptions.create("ProductChannel", {
+  connected: function() {},
+  // Called when the subscription is ready for use on the server
 
   disconnected: function() {},
-    // Called when the subscription has been terminated by the server
+  // Called when the subscription has been terminated by the server
 
   received: function(data) {
   // Called when there's incoming data on the websocket for this channel
-  $(".alert.alert-info").show();
-  $('.product-reviews').prepend(data.comment);
-  $("#average-rating").attr('data-score', data.average_rating);
-  refreshRating();
-},
+    $(".alert.alert-info").show();
+    $('.product-reviews').prepend(data.comment);
+    $("#average-rating").attr('data-score', data.average_rating);
+    refreshRating();
+  },
 
-listen_to_comments: function() {
-  return this.perform('listen', {
-    product_id: $("[data-product-id]").data("product-id")
-  });
-}
+  listen_to_comments: function() {
+    return this.perform('listen', {
+      product_id: $("[data-product-id]").data("product-id")
+    });
+  }
 });
 
 $(document).on('turbolinks:load', function() {
