987900d3f0f4875f883072b8b50512767ae681d6
EC2 Default User <ec2-user@ip-172-31-42-163.us-east-2.compute.internal>
2018-11-02 03:32:45 +0000

    redirect

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'simple_pages#landing_page'
   post 'simple_pages/thank_you'
+  post '/payments/create' => 'payments#create'
   
   mount ActionCable.server => '/cable'
 end
